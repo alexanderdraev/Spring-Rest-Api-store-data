b10117f26a66a7699a5b6e0d19807f6be17b2d43
alexanderdraev <alexanderdraev@gmail.com>
2019-05-27 10:59:28 +0300

    Initial commit.

---
+++ src/main/java/com/example/SpringRestApistoredata/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.example.SpringRestApistoredata.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String exchangeName = "exchange-name";
+
+    static final String queueName = "queue-name";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("routing.key.name");
+    }
+
+  
+}
--- src/main/java/com/example/SpringRestApistoredata/service/ProductService.java
+++ src/main/java/com/example/SpringRestApistoredata/service/ProductService.java
@@ -1,31 +1,38 @@
 package com.example.SpringRestApistoredata.service;
 
+import com.example.SpringRestApistoredata.config.RabbitMQConfig;
 import com.example.SpringRestApistoredata.dto.ProductDTO;
 import com.example.SpringRestApistoredata.model.Product;
 import com.example.SpringRestApistoredata.repository.ProductRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
-    @Autowired
+    private final RabbitTemplate rabbitTemplate;
+
     private ProductRepository productRepository;
 
-    @Autowired
-    ModelMapper modelMapper;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(RabbitTemplate rabbitTemplate, ProductRepository productRepository, ModelMapper modelMapper) {
+        this.rabbitTemplate = rabbitTemplate;
         this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
     }
+
 
     public Product create(ProductDTO productDTO) {
 
-        System.out.println(productDTO);
         Product product = modelMapper.map(productDTO, Product.class);
         productRepository.save(product);
+
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(RabbitMQConfig.exchangeName, "routing.key.name", "Hello, hello from RabbitMQ!");
 
         return product;
     }
